0a888fc6822753621b6f324e851b829d4b35fdfa
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-09-21 19:45:19 +0300

    start making SongsCollectionViewController

---
+++ Jacamole/SongsCollectionViewController.swift
@@ -0,0 +1,79 @@
+//
+//  SongsCollectionViewController.swift
+//  Jacamole
+//
+//  Created by ALEKSEY SUSLOV on 21.09.2022.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class SongsCollectionViewController: UICollectionViewController {
+    
+    private let cellID = "Cell"
+    private var songsList: [String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        
+    }
+}
+
+
+// MARK: UICollectionViewDataSource
+extension SongsCollectionViewController {
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    
+        // Configure the cell
+    
+        return cell
+    }
+}
+
+
+// MARK: UICollectionViewDelegate
+extension SongsCollectionViewController {
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+}
