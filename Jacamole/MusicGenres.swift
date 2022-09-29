6951ac717f3a1c3ad2fa58eb644b04be3d6db92b
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-09-29 21:34:55 +0300

    add MusicGenres enumeration and posters of music genres

--- Jacamole/MusicGenres.swift
+++ Jacamole/MusicGenres.swift
@@ -5,8 +5,29 @@
 //  Created by ALEKSEY SUSLOV on 29.09.2022.
 //
 
-import Foundation
-
-enum MusicGenres {
-    
+enum MusicGenres: String, CaseIterable {
+    case soul
+    case pop
+    case dreampop
+    case electropop
+    case rock
+    case electronic
+    case dance
+    case edm
+    case hiphop
+    case jazz
+    case funk
+    case indie
+    case filmscore
+    case classical
+    case chillout
+    case ambient
+    case folk
+    case metal
+    case latin
+    case rnb
+    case reggae
+    case country
+    case house
+    case blues
 }
